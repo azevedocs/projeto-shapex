a561b0fb88be88b180081d5a7c8760a6c057b9c0
azevedocs <gabriel_azevedocs@outlook.com>
2024-03-31 14:42:09 -0300

    31/03 - js CARRINHO

--- js/app.js
+++ js/app.js
@@ -15,21 +15,71 @@ produtos.eventos = {
 produtos.metodos = {
 
     // obtem a lista de itens dos produtos
-    obterItensProdutos: () => {
+    obterItensProdutos: (categoria = 'proteinas', vermais = false) => {
 
-        var filtro = MENU['proteina'];
+        var filtro = MENU[categoria];
         console.log(filtro)
+
+        if (!vermais) {
+            $("#itensProdutos").html('');
+            $("#btnVerMais").removeClass('hidden');
+        }
 
         $.each(filtro, (i, e) => {
 
             let temp = produtos.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
+            .replace(/\${id}/g, e.id)
 
-            $("#itensProdutos").append(temp)
+            // botão ver mais foi clicado (12 itens)
+            if (vermais && i >= 8 && i < 12) {
+                $("#itensProdutos").append(temp)
+            }
+
+            // paginação inicial (8 itens)
+            if (!vermais && i < 8) {
+                $("#itensProdutos").append(temp)
+            }
 
         })
+
+        //remove o ativo
+        $(".container-menu a").removeClass('active');
+
+        //seta o menu para ativo
+        $("#menu-" + categoria).addClass('active');
+
     },
+
+    //clique no botão ver mais
+    verMais: () => {
+
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];
+        produtos.metodos.obterItensProdutos(ativo, true)
+
+        $("#btnVerMais").addClass('hidden');
+
+    },
+
+    // diminuir a quantidade do item nos produtos
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+
+        if (qntdAtual > 0) {
+
+            $("#qntd-" + id).text(qntdAtual - 1)
+        }
+
+    },
+
+    // aumentar a quantidade do item nos produtos
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1)
+    }
 
 }
 
@@ -37,7 +87,7 @@ produtos.templates = {
 
     item: `
         <div class="col-3 mb-5">
-            <div class="card card-item">
+            <div class="card card-item" id"\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
                 </div>
@@ -48,9 +98,9 @@ produtos.templates = {
                     <b>R$ \${preco}</b>
                 </p>
                 <div class="add-carrinho">  
-                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
-                    <span class="add-numero-itens">0</span>
-                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
+                    <span class="btn-menos" onclick="produtos.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" onclick="produtos.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
                     <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
                 </div>
             </div>
