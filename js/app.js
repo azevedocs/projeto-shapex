1e76dc68159028b07eeacec4d27703eef0dba197
azevedocs <gabriel_azevedocs@outlook.com>
2024-04-09 12:12:12 -0300

    09/04/2024 -

--- js/app.js
+++ js/app.js
@@ -114,15 +114,82 @@ produtos.metodos = {
                     MEU_CARRINHO.push(item[0])
                 }
 
-                
+                produtos.metodos.mensagem('Item adicionado ao carrinho','green')
                 $("#qntd-" + id).text(0);
 
-
+                produtos.metodos.atualizarBadgeTotal();
+                
             }
 
         }
 
     },
+
+    // atualiza o bagde de totais dos botões "Meu carrinho"
+    atualizarBadgeTotal: () => {
+
+        var total = 0;
+
+        $.each(MEU_CARRINHO, (i, e) => {
+            total += e.qntd
+        })
+
+        if (total > 0) {
+            $(".botao-carrinho").removeClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+        }
+        else {
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").addClass('hidden');
+        }
+
+        $(".badge-total-carrinho").html(total);
+
+    },
+
+    //abrir a modal de carrinho
+    abrirCarrinho: (abrir) => {
+
+        if(abrir) {
+            $("#modalCarrinho").removeClass('hidden');
+        }
+        else {
+            $("#modalCarrinho").addClass('hidden');
+        }
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Mensagens
+    mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+
+        let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+
+        $("#container-mensagens").append(msg);
+
+        setTimeout(() => {
+            $("#msg-" + id).removeClass('fadeInDown');
+            $("#msg-" + id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $("#msg-" + id).remove();
+            }, 800)
+        }, tempo)
+
+    }
 
 }
 
