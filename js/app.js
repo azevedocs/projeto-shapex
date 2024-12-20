8bf939d87f9db861d984158d17a672641ee0931d
Gabriel <gabriel_azevedocs@outlook.com>
2024-12-20 17:11:55 -0300

    terminando a responsividade p1

--- js/app.js
+++ js/app.js
@@ -563,7 +563,7 @@ produtos.metodos = {
 produtos.templates = {
 
     item: `
-        <div class="col-3 mb-5 animated fadeInUp">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
             <div class="card card-item" id"\${id}">
                 <div class="img-produto">
                     <img src="\${img}" />
